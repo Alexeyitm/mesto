57851dea57190fa0e87687c0638abe182f84a311
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-02-18 22:02:31 +0500

    fix: fix bags in all sections

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,37 @@
-// select buttons
-let buttonEdit = document.querySelector('.profile__button-edit');
-let buttonClose = document.querySelector('.popup__button-close');
-let buttonHearts = document.querySelectorAll('.element__svg-heart');
+// select buttons, elements, inputs and form
+let editButton = document.querySelector('.profile__button-edit');
+let closeButton = document.querySelector('.popup__button-close');
+let heartsButton = document.querySelectorAll('.element__svg-heart');
 let buttonAdd = document.querySelector('.popup__button-add');
-
-// select forms
-let popup = document.querySelector('.popup');
-let name = document.querySelector('.profile__name');
-let text = document.querySelector('.profile__text');
-let inputs = document.querySelectorAll('.popup__input');
+let popupElement = document.querySelector('.popup');
+let nameElement = document.querySelector('.profile__name');
+let textElement = document.querySelector('.profile__text');
+let nameInput = document.querySelectorAll('.popup__input-name');
+let jobInput = document.querySelectorAll('.popup__input-job');
+let formElement = document.querySelectorAll('.popup__form');
 
 // open popup
-buttonEdit.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
-  inputs[0].setAttribute('value', name.textContent);
-  inputs[1].setAttribute('value', text.textContent);
+editButton.addEventListener('click', function () {
+  popupElement.classList.add('popup_opened');
 });
 
 // close popup
-buttonClose.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
+closeButton.addEventListener('click', function () {
+  popupElement.classList.remove('popup_opened');
 });
 
 // change name and text
-buttonAdd.addEventListener('click', function () {
-  name.textContent = inputs[0].value;
-  text.textContent = inputs[1].value;
-  popup.classList.add('popup_opened');
-});
+//buttonAdd.addEventListener('click', function () {
+  //nameElement.textContent = nameInput.value;
+  //textElement.textContent = jobInput.value;
+  //popupElement.classList.remove('popup_opened');
+//});
 
 // add "likes"
-for (let buttonHeart of buttonHearts) {
-  buttonHeart.addEventListener('click', function () {
-    buttonHeart.setAttribute('style', 'background-image: url(./image/Vector-heart-black.svg);');
-    buttonHeart.classList.remove('element__svg-heart_hover');
+for (let heartButton of heartsButton) {
+  heartsButton.addEventListener('click', function () {
+    heartsButton.setAttribute('style', 'background-image: url(./image/Vector-heart-black.svg);');
+    heartsButton.classList.remove('element__svg-heart_hover');
   });
 }
 
