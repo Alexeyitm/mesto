73112789af5c502724fffce34ec201654102bd13
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-02-19 10:14:05 +0500

    fix: fix js script

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,17 @@ let buttonAdd = document.querySelector('.popup__button-add');
 let popupElement = document.querySelector('.popup');
 let nameElement = document.querySelector('.profile__name');
 let textElement = document.querySelector('.profile__text');
-let nameInput = document.querySelectorAll('.popup__input-name');
-let jobInput = document.querySelectorAll('.popup__input-job');
-let formElement = document.querySelectorAll('.popup__form');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+let formElement = document.querySelector('.popup__form');
+
+console.log(formElement)
 
 // open popup
 editButton.addEventListener('click', function () {
   popupElement.classList.add('popup_opened');
+  nameInput.value = nameElement.textContent;
+  jobInput.value = textElement.textContent;
 });
 
 // close popup
@@ -21,11 +25,11 @@ closeButton.addEventListener('click', function () {
 });
 
 // change name and text
-//buttonAdd.addEventListener('click', function () {
-  //nameElement.textContent = nameInput.value;
-  //textElement.textContent = jobInput.value;
-  //popupElement.classList.remove('popup_opened');
-//});
+formElement.addEventListener('submit', function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameElement.textContent = nameInput.value;
+  textElement.textContent = jobInput.value;
+});
 
 // add "likes"
 for (let heartButton of heartsButton) {
