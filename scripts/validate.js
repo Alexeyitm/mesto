bdcbdbd68a705aa84455b9644431df2efa8343ba
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-03-25 20:08:44 +0500

    feat: add validation popupAdd

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,11 @@
 function enanleValidation() {
-  const form = document.querySelector('.popup__form_edit');
-  
-  form.addEventListener('input', handleFormInput);
+  const formEdit = document.querySelector('.popup__form_edit');
+  const formAdd = document.querySelector('.popup__form_add');
+
+  formEdit.addEventListener('input', handleFormInput);
+  formAdd.addEventListener('input', handleFormInput);
 }
+
 
 function handleFormInput(event) {
   const form = event.currentTarget;
@@ -10,21 +13,26 @@ function handleFormInput(event) {
 
   setCustomError(input);
   setFieldError(input);
+  setSubmitButtonState(form);
 }
 
 function setCustomError(input) {
   const validity = input.validity;
   const currentLength = input.value.length;
 
-  input.setCustomValidity('')
+  input.setCustomValidity('');
 
   if (currentLength == 0) {
-    input.setCustomValidity('Вы пропустили это поле.')
+    input.setCustomValidity('Вы пропустили это поле.');
   }
 
   if (validity.tooShort || validity.tooLong) {
     const min = input.getAttribute('minlength');
     input.setCustomValidity(`Минимальное количество символов: ${min}. Длина текста сейчас: ${currentLength} символ.`);
+  }
+
+  if (validity.typeMismatch) {
+    input.setCustomValidity(`Введите адрес сайта`);
   }
 }
 
@@ -33,6 +41,20 @@ function setFieldError(input) {
   span.textContent = input.validationMessage;
 }
 
+function setSubmitButtonState(form) {
+  const button = form.querySelector('.popup__button-add');
+  const isValid = form.checkValidity();
+
+  if (isValid) {
+    button.classList.remove('popup__button-add_invalid');
+    button.removeAttribute('disabled');
+  }
+  else {
+    button.classList.add('popup__button-add_invalid');
+    button.setAttribute('disabled', 'disabled');
+  }
+}
 
 
-enanleValidation()
+
+enanleValidation();
